8ed5c02c1628b624763387a588056505eed1376a
prajeeth-12 <prajeeth12121@gmail.com>
2026-08-02 23:08:24 +0530

    Remove all files

--- hr-orchestrate_aug_26/_vendor/python-clients/scripts/nmt/__init__.py
+++
@@ -1,2 +0,0 @@
-#q SPDX-FileCopyrightText: Copyright (c) 2022 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-# SPDX-License-Identifier: MIT
--- hr-orchestrate_aug_26/code/features/__init__.py
+++
@@ -1,8 +0,0 @@
-# Feature extraction modules for Message Notification Router
-
-from .user_features import UserHistoryFeatureExtractor, create_feature_extractor
-
-__all__ = [
-    'UserHistoryFeatureExtractor',
-    'create_feature_extractor'
-]
